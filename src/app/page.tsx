30e8fd13b8d5a36005cdcd79fb14210bf401d756
SKH-TMS <k.s.h.taskmanagement@gmail.com>
2024-10-06 09:26:54 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-center text-2xl my-5">
+        First Basic Application on NestJS
+      </h1>
+      <p className="text-center">
+        I am a web developer and learning web development on base of awesome
+        framework NestJS. This framework is of JavaScript.{" "}
+      </p>
+    </div>
+  );
+}
